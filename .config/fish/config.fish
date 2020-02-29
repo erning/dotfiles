137867259d34f1ea61faf98d28c52100980eb3c1
Zhang Erning <zhang@erning.com>
2020-03-01 05:16:33 +0800

    less color scheme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -51,6 +51,15 @@ alias cp 'cp -i'
 set -x LANG "en_US.UTF-8"
 set -x LC_ALL "en_US.UTF-8"
 
+# less color scheme
+set -x LESS_TERMCAP_mb (printf '\e[1;32m')
+set -x LESS_TERMCAP_md (printf '\e[1;32m')
+set -x LESS_TERMCAP_me (printf '\e[0m')
+set -x LESS_TERMCAP_se (printf '\e[0m')
+set -x LESS_TERMCAP_so (printf '\e[01;33m')
+set -x LESS_TERMCAP_ue (printf '\e[0m')
+set -x LESS_TERMCAP_us (printf '\e[1;4;31m')
+
 
 #
 # prompt
