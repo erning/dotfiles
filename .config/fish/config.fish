b3b005de6ef82e9090264d27ad79203574edfb0e
Zhang Erning <zhang@erning.com>
2020-02-29 21:10:14 +0800

    functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -43,22 +43,6 @@ alias rm 'rm -i'
 alias mv 'mv -i'
 alias cp 'cp -i'
 
-alias spacevim "env SHELL=/bin/bash nvim -u $HOME/.SpaceVim/vimrc"
-
-
-# colorful manual pages
-function man
-    env \
-        LESS_TERMCAP_mb=(printf "\e[1;31m") \
-        LESS_TERMCAP_md=(printf "\e[1;31m") \
-        LESS_TERMCAP_me=(printf "\e[0m") \
-        LESS_TERMCAP_se=(printf "\e[0m") \
-        LESS_TERMCAP_so=(printf "\e[1;44;33m") \
-        LESS_TERMCAP_ue=(printf "\e[0m") \
-        LESS_TERMCAP_us=(printf "\e[1;32m") \
-        man $argv
-end
-
 # locale
 set -x LANG "en_US.UTF-8"
 set -x LC_ALL "en_US.UTF-8"
---
+++ .config/fish/functions/spacevim.fish
@@ -0,0 +1,4 @@
+function spacevim
+    # env SHELL=/bin/bash 
+    nvim -u $HOME/.SpaceVim/vimrc
+end
