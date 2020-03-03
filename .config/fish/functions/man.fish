25c58a4bb3a21b8d500087f1e0d0bd215f7b009c
Zhang Erning <zhang@erning.com>
2020-03-03 21:41:27 +0800

    colorful man by fish function

---
+++ .config/fish/functions/man.fish
@@ -0,0 +1,54 @@
+#
+# from ($__fish_data_dir}/functions/man.fish
+#
+
+if not command -qs man
+    # see #5329 and discussion at https://github.com/fish-shell/fish-shell/commit/13e025bdb01cc4dd08463ec497a0a3495873702f
+    exit
+end
+
+function man --description "Format and display the on-line manual pages"
+    # Work around the "builtin" manpage that everything symlinks to,
+    # by prepending our fish datadir to man. This also ensures that man gives fish's
+    # man pages priority, without having to put fish's bin directories first in $PATH.
+
+    # Preserve the existing MANPATH, and default to the system path (the empty string).
+    set -l manpath
+    if set -q MANPATH
+        set manpath $MANPATH
+    else if set -l p (command man -p 2>/dev/null)
+        # NetBSD's man uses "-p" to print the path.
+        # FreeBSD's man also has a "-p" option, but that requires an argument.
+        # Other mans (men?) don't seem to have it.
+        #
+        # Unfortunately NetBSD prints things like "/usr/share/man/man1",
+        # while not allowing them as $MANPATH components.
+        # What it needs is just "/usr/share/man".
+        #
+        # So we strip the last component.
+        # This leaves a few wrong directories, but that should be harmless.
+        set manpath (string replace -r '[^/]+$' '' $p)
+    else
+        set manpath ''
+    end
+    # Notice the shadowing local exported copy of the variable.
+    set -lx MANPATH $manpath
+
+    # Prepend fish's man directory if available.
+    set -l fish_manpath (dirname $__fish_data_dir)/fish/man
+    if test -d $fish_manpath
+        set MANPATH $fish_manpath $MANPATH
+    end
+
+    # command man $argv
+    env \
+        LESS_TERMCAP_mb=(printf '\e[1;32m') \
+        LESS_TERMCAP_md=(printf '\e[1;32m') \
+        LESS_TERMCAP_me=(printf '\e[0m') \
+        LESS_TERMCAP_se=(printf '\e[0m') \
+        LESS_TERMCAP_so=(printf '\e[01;33m') \
+        LESS_TERMCAP_ue=(printf '\e[0m') \
+        LESS_TERMCAP_us=(printf '\e[1;4;31m') \
+        man $argv
+end
+
