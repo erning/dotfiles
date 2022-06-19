e4dd2dd1ffe09e1636d29db5eb9389914e2fb984
Zhang Erning <zhang@erning.com>
2022-06-20 01:07:00 +0800

    fish 3.5.0

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,7 @@ if command -qs brew; and test -f (brew --prefix)/opt/fzf/shell/key-bindings.fish
     source (brew --prefix)/opt/fzf/shell/key-bindings.fish
 end
 
-if functions fzf_key_bindings
+if functions -q fzf_key_bindings
     function fish_user_key_bindings
         fzf_key_bindings
     end
