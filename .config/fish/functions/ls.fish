5e74fcbbe7b44f62986c901aeea8e869494e6d3b
Zhang Erning <zhang@erning.com>
2023-10-19 13:57:35 +0800

    exa => eza

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,6 +1,6 @@
-if command -qs exa
-    function ls --description "alias ls=exa -F"
-        command exa -F $argv
+if command -qs eza
+    function ls --description "alias ls=eza -F"
+        command eza -F $argv
     end
 else
     source $__fish_data_dir/functions/ls.fish
