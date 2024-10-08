74d4262602c1c84284be90136a5212780d64f031
Zhang Erning <zhang@erning.com>
2024-10-08 11:44:45 +0800

    ln -s nvim-lazyvim nvim

--- .config/nvim-lazyvim/lua/config/keymaps.lua
+++ .config/nvim-lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+if vim.lsp.inlay_hint then
+  LazyVim.toggle.map("<C-S-h>", LazyVim.toggle.inlay_hints)
+end
--- .config/nvim-lazyvim/lua/plugins/lang-python.lua
+++ .config/nvim-lazyvim/lua/plugins/lang-python.lua
@@ -1,3 +1,7 @@
+if true then
+  return {}
+end
+
 return {
   -- add pyright to lspconfig
   {
