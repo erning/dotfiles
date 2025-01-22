7c4d5c4d4807067b4c3b39af29754266097f4f06
Zhang Erning <zhang@erning.com>
2025-01-22 13:32:16 +0800

    lazyvim

--- .config/nvim-lazyvim/lua/config/options.lua
+++ .config/nvim-lazyvim/lua/config/options.lua
@@ -2,8 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- vim.g.lazyvim_python_lsp = "basedpyright"
-
 if vim.g.neovide then
   vim.o.guifont = "JetBrainsMono Nerd Font Mono,LXGW WenKai Mono:h17"
 end
